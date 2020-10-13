f24b538584d1f2d4dda9dfbcb0d7590a79c5b0a5
Vahe Gemilyan <vahe.gemilyan@gmail.com>
2020-10-12 20:24:07 -0700

    Initial commit

---
+++ src/Lesson01/Main.java
@@ -0,0 +1,17 @@
+package Lesson01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 24;
+
+        System.out.println("Сложение: " + (a+b) );
+        System.out.println("Умножение: " + (a*b) );
+        System.out.println("Вычитание: " + (b-a) );
+        System.out.println("Деление: " + (double) b/a);
+        System.out.println("Остаток от деления: " + (b%a) );
+
+        System.out.println("\uD83D\uDE40");
+    }
+}
