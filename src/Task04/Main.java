85594d9b5e506bcce33f6844b9acf63c5c6db227
Vahe Gemilyan <vahe.gemilyan@gmail.com>
2020-10-17 15:58:55 -0700

    Task 04 additional

--- src/Task04/Main.java
+++ src/Task04/Main.java
@@ -1,5 +1,7 @@
 package Task04;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +11,7 @@ public class Main {
 
         System.out.println("Task 1");
 
-        for (int i = 0; i <= 15; i++){
+        for (int i = 0; i <= 15; i++) {
             System.out.print(i + " ");
         }
 
@@ -21,8 +23,8 @@ public class Main {
 
         System.out.println("\nTask 2");
 
-        for (int y = 5; y < 10000; y = y * 5){
-            System.out.print(y + " ");
+        for (int j = 5; j < 10000; j = j * 5) {
+            System.out.print(j + " ");
         }
 
 
@@ -35,8 +37,8 @@ public class Main {
 
         System.out.println("\nTask 3 take 1");
 
-        for (int a = 40; a<=60; a++){
-            if (a%4==0) {
+        for (int a = 40; a <= 60; a++) {
+            if (a % 4 == 0) {
                 System.out.print(a + " ");
             }
         }
@@ -44,11 +46,94 @@ public class Main {
 
         System.out.println("\nTask 3 take 2");
 
-        for (int b=40; b<=60; b=b+4){
+        for (int b = 40; b <= 60; b = b + 4) {
             System.out.print(b + " ");
+        }
+
+        /*Task 1*
+        Find if the number is 2 digits 3 digits or 4 digits and
+        check the result for the next condition use if and else:
+        Its a two digit number
+        Its a tree digit number
+        Its a four digit number
+        Its a five digit number
+        The number is not between 1 & 99999*/
+
+        System.out.println("\nTask 1*");
+
+        int a = 12345;
+        int num = a;
+        int count = 0;
+
+        for(; num != 0; num/=10, ++count) {
+        }
+
+        System.out.println("The number of digits is " + count);
+
+        if (count == 2){
+            System.out.println(a + " is a two digit number");
+        }else if (count == 3) {
+            System.out.println(a + " is a three digit number");
+        }else if (count == 4) {
+            System.out.println(a + " is a four digit number");
+        }else if (count == 5) {
+            System.out.println(a + " is a five digit number");
+        }else {
+            System.out.println("The number " + a + " is not between 1 & 99999");
+        }
+
+       /* Task 2*
+        Write a program to find if the use largest Number
+        of three number print out System.out.println("Largest number is "+largestNumber);
+        Must be used if then an else if find lastly else*/
+
+        System.out.println("\nTask 2*");
+
+        int x = 100;
+        int y = 200;
+        int z = 300;
+
+        if( x >= y && x >= z) {
+            System.out.println("The largest number is " + x);
+        }else if (y >= x && y >= z) {
+            System.out.println("The largest number is " + y);
+        }else{
+            System.out.println("The largest number is " + z);
+    }
+
+
+        /*Task 3*
+        Create a program print only even numbers from 1 to 20 must be used while loops*/
+
+        System.out.println("\nTask 3*");
+
+        int c = 1;
+        while (c <= 10) {
+            System.out.print(c * 2 + " ");
+            c++;
+        }
+
+        /*Task 4
+        Create a program for print numbers from 10 to 1*/
+
+        System.out.println("\nTask 4*");
+
+        for (int k = 10; k > 0; k--){
+            System.out.print(k + " ");
         }
 
 
 
+
+
+
+
+
     }
-}
+
+
+    }
+
+
+
+
