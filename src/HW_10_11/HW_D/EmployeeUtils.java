915f85d1394dc6ce9ac6c1f1a33120fd3eeba048
Vahe Gemilyan <vahe.gemilyan@gmail.com>
2020-12-02 17:58:46 -0800

    Homework 11

--- src/HW_10_11/HW_D/EmployeeUtils.java
+++ src/HW_10_11/HW_D/EmployeeUtils.java
@@ -1,11 +1,12 @@
 package HW_10_11.HW_D;
 
-/*  Необходимо создать утилитарный класс со следующими методами:
+    /*  Необходимо создать утилитарный класс со следующими методами:
     поиск сотрудника в массиве по его имени */
 
 import HW_9.Employee;
 
 public class EmployeeUtils {
+
 public static Employee searchByName(Employee[] employees, String name){
 
     for (int i = 0; i < employees.length; i++){
@@ -30,5 +31,5 @@ public static Employee getMinSalary(Employee[] employees){
         }
     }
     return result;
-}
+    }
 }
